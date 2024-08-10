8648fc5eea992e6808c6163265a3420d147afb45
Diana Hernandez <diana.hernandez@xing.com>
2024-08-10 10:39:10 +0200

    Add date

--- mitarrito/Components/StickyView.swift
+++ mitarrito/Components/StickyView.swift
@@ -3,20 +3,23 @@ import SwiftUI
 struct StickyView: View {
     var text: String
     var color: Color
+    var date: Date
 
-    init(text: String = "¡Hola, mundo!") {
-            self.text = text
-        	self.color =  Color(
-                hue: Double.random(in: 0...1),
-                saturation: Double.random(in: 0.3...0.7),
-                brightness: Double.random(in: 0.7...1)
-            )
-        }
+    init(text: String = "¡Hola, mundo!", date: Date = Date()) {
+        self.text = text
+        self.date = date
+        self.color =  Color(
+            hue: Double.random(in: 0...1),
+            saturation: Double.random(in: 0.3...0.7),
+            brightness: Double.random(in: 0.7...1)
+        )
+    }
 
     var body: some View {
         VStack {
             Spacer()
             ZStack(alignment: .topTrailing) {
+
                 Image("postit")
                     .resizable()
                     .colorMultiply(color)
@@ -25,7 +28,12 @@ struct StickyView: View {
                 Text(text)
                     .font(.largeTitle)
                     .frame(width: 300, height: 300)
+                Text(date, style: .date)
+                    .font(.caption2)
+                    .padding(8)
+                    .offset(x: -35, y: 35)
             }
+
         }
         .frame(width: 200, height: 200)
     }
@@ -33,24 +41,4 @@ struct StickyView: View {
 
 #Preview {
     StickyView()
-}
-
-struct BentCornerShape: Shape {
-    var corner: UIRectCorner
-    var size: CGFloat
-
-    func path(in rect: CGRect) -> Path {
-        var path = Path()
-        let width = rect.size.width
-        let height = rect.size.height
-
-        path.move(to: CGPoint(x: 0, y: 0))
-        path.addLine(to: CGPoint(x: width - size, y: 0))
-        path.addLine(to: CGPoint(x: width, y: size))
-        path.addLine(to: CGPoint(x: width, y: height))
-        path.addLine(to: CGPoint(x: 0, y: height))
-        path.closeSubpath()
-
-        return path
-    }
 }
